9609685a029774e6cacb812e8d82ecf5ab8a1145
zaurh035 <103025471+zaurh035@users.noreply.github.com>
2022-04-05 20:50:17 +0400

    teze

---
+++ My Prayekt1/Assets/C#/MouseLook.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+	public PlayerController playerScript;
+
+	[Range(50, 500)]
+	public float sens;
+	public Transform body;
+
+	float xRot = 0f;
+
+	public Transform leaner;
+	public float zRot;
+	bool canRotate = true;
+	bool isRotating;
+
+	public float smoothing;
+	float currentRot;
+
+	private void Start()
+	{
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	private void Update()
+	{
+		#region Camera Movement
+		float rotX = Input.GetAxisRaw("Mouse X") * sens * Time.deltaTime;
+		float rotY = Input.GetAxisRaw("Mouse Y") * sens * Time.deltaTime;
+
+		xRot -= rotY;
+		xRot = Mathf.Clamp(xRot, -80f, 80f);
+
+		currentRot += rotX;
+		currentRot = Mathf.Lerp(currentRot, 0, smoothing * Time.deltaTime);
+
+		if (canRotate) 
+		{ 
+			transform.localRotation = Quaternion.Euler(xRot, 0f, currentRot);
+			body.Rotate(Vector3.up * rotX);
+		}
+
+		#endregion
+
+		#region Camera Lean
+		if (Input.GetKey(KeyCode.E))
+		{
+			playerScript.isLeaning = true;
+			zRot = Mathf.Lerp(zRot, -20.0f, 5f * Time.deltaTime);
+			isRotating = true;
+			canRotate = false;
+			playerScript.canMove = false;
+		}
+
+		if (Input.GetKey(KeyCode.Q))
+		{
+			playerScript.isLeaning = true;
+			zRot = Mathf.Lerp(zRot, 20.0f, 5f * Time.deltaTime);
+			isRotating = true;
+			canRotate = false;
+			playerScript.canMove = false;
+		}
+
+		if (Input.GetKeyUp(KeyCode.Q) || Input.GetKeyUp(KeyCode.E))
+		{
+			playerScript.isLeaning = false;
+			isRotating = false;
+			canRotate = true;
+			playerScript.canMove = true;
+		}
+
+		if (!isRotating)
+			zRot = Mathf.Lerp(zRot, 0.0f, 5f * Time.deltaTime);
+
+		leaner.localRotation = Quaternion.Euler(0, 0, zRot);
+		#endregion
+	}
+}
---
+++ My Prayekt1/Assets/C#/PlayerController.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	CharacterController controller;
+	AudioSource source;
+
+	Vector3 velocity;
+	bool isGrounded;
+	bool isMoving;
+	public bool isLeaning;
+
+	public Transform ground;
+	public float distance = 0.3f;
+
+	public float speed;
+	public float jumpHeight;
+	public float gravity;
+
+	public float originalHeight;
+	public float crouchHeight;
+
+	public bool canMove = true;
+
+	public LayerMask mask;
+
+	public AudioClip[] defaultSounds;
+	public AudioClip[] grassSounds;
+	public AudioClip[] snowSounds;
+
+	int soundControl;
+
+	// 0 = Default
+	// 1 = Grass
+	// 2 = Snow
+
+	public float timeBetweenSteps;
+	float timer;
+
+	private void Start()
+	{
+		controller = GetComponent<CharacterController>();
+		source = GetComponent<AudioSource>();
+	}
+
+	private void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		//switch (hit.transform.tag)
+		//	{
+		//	case "Ground": soundControl = 0; break;
+		//	case "Grass":  soundControl = 1; break;
+		//		case "Snow":   soundControl = 2; break;
+		//	}
+	}
+
+	private void Update()
+	{
+		#region Movement
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		
+		Vector3 move = transform.right * horizontal + transform.forward * vertical;
+
+		if(canMove)
+			controller.Move(move * speed * Time.deltaTime);
+		#endregion
+
+		//	#region Footsteps
+		//	if (horizontal != 0 || vertical != 0)
+		//		isMoving = true;
+		//else
+		//isMoving = false;
+
+		//// (isMoving && isGrounded && !isLeaning)
+		//
+		// -= Time.deltaTime;
+
+		//(timer <= 0)
+		//
+		//switch (soundControl)
+		//	{
+		//	case 0:	source.clip = defaultSounds[Random.Range(0, defaultSounds.Length)];	break;
+		//v		case 1:	source.clip = grassSounds[Random.Range(0, grassSounds.Length)];	    break;
+		//		case 2:	source.clip = snowSounds[Random.Range(0, snowSounds.Length)];	    break;
+		//	}
+
+		//	timer = timeBetweenSteps;
+		//source.pitch = Random.Range(0.85f, 1.15f);
+		//		source.Play();
+		//		//}
+		//	}
+		//	else
+		//	{
+		//		timer = timeBetweenSteps;
+		//	}
+
+		//	#endregion
+
+		#region Jump
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !isLeaning)
+		{
+			velocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+		}
+		#endregion
+
+		#region Gravity
+		isGrounded = Physics.CheckSphere(ground.position, distance, mask);
+
+		if(isGrounded && velocity.y < 0)
+		{
+			velocity.y = -2f;
+		}
+
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move(velocity * Time.deltaTime);
+		#endregion
+
+		#region Basic Crouch
+		if (Input.GetKeyDown(KeyCode.LeftControl))
+		{
+			controller.height = crouchHeight;
+			speed = 2.0f;
+			jumpHeight = 0;
+		}
+
+		if (Input.GetKeyUp(KeyCode.LeftControl))
+		{
+			speed = 5.0f;
+			jumpHeight = 2;
+			controller.height = originalHeight;
+		}
+		#endregion
+
+		#region Basic Running
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			speed = 7.0f;
+			timeBetweenSteps = 0.3f;
+		}
+
+		if (Input.GetKeyUp(KeyCode.LeftShift))
+		{
+			speed = 5.0f;
+			timeBetweenSteps = 0.5f;
+		}
+		#endregion
+	}
+}
